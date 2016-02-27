d3ec86a931517733e59680da0f9fdef9ea732a8f
lordgiotto <lordgiotto@gmail.com>
2016-02-27 20:57:27 +0100

    Small workflow modifications

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@
 // -----------------------------------------------------------------------------
 
 var path           = require('path');
+var fs             = require('fs');
 var gulp           = require('gulp');
 var connect        = require('gulp-connect');
 
@@ -33,6 +34,7 @@ var buffer         = require('vinyl-buffer');
 
 // Gulp plugins
 var gutil          = require('gulp-util');
+var watch          = require('gulp-watch');
 var gulpif         = require('gulp-if');
 var cssnano        = require('gulp-cssnano');
 var notifier       = require('node-notifier');
@@ -42,10 +44,11 @@ var imagemin       = require('gulp-imagemin');
 var pngquant       = require('imagemin-pngquant');
 var uglify         = require('gulp-uglify');
 var cache          = require('gulp-cache');
+var sourcemaps     = require('gulp-sourcemaps');
 var livereload     = require('gulp-livereload');
 
 // Configs
-var cssVars        = require('./cssvars.json');
+var gulpConfig        = require('./gulpConfig.json');
 
 // -----------------------------------------------------------------------------
 // Configuration
@@ -53,38 +56,41 @@ var cssVars        = require('./cssvars.json');
 
 var argv = require('yargs').argv;
 
-const filePaths = {
-	src: {
-		base:   'src/',
-		css:    'src/css/',
-		js:     'src/js/main.js',
-		img:    'src/rawimg/',
-		svg:    'src/svg/'
-	},
-	dest: {
-		base:     'dist/',
-		css:      'dist/css/',
-		js:       'dist/js/',
-		imported: 'dist/imported',
-		img:      'dist/img/'
-	}
-}
+const filePaths = gulpConfig.filePaths;
+
+const autoprefixConfig = gulpConfig.autoprefixConfig;
 
 var production = argv.prod || false;
+
+// -----------------------------------------------------------------------------
+// Debug
+// -----------------------------------------------------------------------------
+
+var debug = argv.debug || false;
+
+if (debug) {
+	console.log('*********************************************');
+	console.log ( gutil.colors.cyan.bold('Filepaths:') );
+	console.log(filePaths);
+	console.log ( gutil.colors.cyan.bold('Autoprefixer config:') );
+	console.log(autoprefixConfig);
+	console.log('*********************************************');
+	process.env.BROWSERIFYSHIM_DIAGNOSTICS=1;
+}
 
 // -----------------------------------------------------------------------------
 // Helpers
 // -----------------------------------------------------------------------------
 
 var onError = function (err) {
+	var errorElements = ['plugin', 'fileName', 'line']
 	console.log('*********************************************');
-	if (err.fileName) {
-		console.log ( gutil.colors.bgRed.white('Filename: ', path.basename(err.fileName)) );
-	} else if (err.plugin) {
-		console.log ( gutil.colors.bgRed.white('Plugin: ', path.basename(err.plugin)) );
-	} else {
-		console.log ( gutil.colors.bgRed.white('Compiation error.') );
-	}
+	console.log ( gutil.colors.bgRed.white('Compiation error.') );
+	errorElements.forEach(function(el){
+		if (err.hasOwnProperty(el)) {
+			console.log ( gutil.colors.red.bold(el.toUpperCase() + ': ', err[el]) );
+		}
+	})
 	console.log ( gutil.colors.white(err.message) );
 	console.log('*********************************************');
 	notifier.notify({
@@ -101,12 +107,6 @@ function startLivereolad() {
 }
 
 // -----------------------------------------------------------------------------
-// Bootstrap
-// -----------------------------------------------------------------------------
-
-
-
-// -----------------------------------------------------------------------------
 // CSS
 // -----------------------------------------------------------------------------
 
@@ -120,10 +120,6 @@ var postcssProcessors = [
 	}),
 	cssnext({
 		features: {
-			customProperties: {
-				variables: cssVars
-			},
-			calc: false,
 			colorHwb: false,
 			autoprefixer: false
 		}
@@ -157,7 +153,8 @@ function buildCss() {
 	del( path.join(filePaths.dest.imported, '*') );
 
 	return gulp.src(path.join(filePaths.src.css, '**/[^_]*.css'))
-	.pipe( postcss(postcssProcessors, {map: !production}) )
+	.pipe( gulpif(!production, sourcemaps.init() ) )
+	.pipe( postcss(postcssProcessors) )
 	.on('error', onError)
 	.pipe( gulpif( production,
 				cssnano({
@@ -167,13 +164,13 @@ function buildCss() {
 			)
 	)
 	.pipe( rename({ suffix: '.min' }) )
+	.pipe( gulpif(!production, sourcemaps.write('.') ) )
 	.pipe( gulp.dest(filePaths.dest.css) )
 	.pipe( livereload() );
 }
 
 function watchCss() {
-	gulp.watch( path.join(filePaths.src.css, '**/*.css'), ['build:css'])
-	gulp.watch( path.join('./cssvars.json'), ['build:css'])
+	watch( path.join(filePaths.src.css, '**/*.css'), ['build:css'] );
 }
 
 gulp.task('build:css', buildCss);
@@ -189,34 +186,48 @@ gulp.task('watch:css', function(){
 // -----------------------------------------------------------------------------
 
 function buildImg() {
-	del( path.join(filePaths.dest.img, '*.{jpg,png,gif,svg}') );
+	del( path.join(filePaths.dest.img, '**/*.{jpg,png,gif,svg}') );
 	return gulp.src( path.join(filePaths.src.img, '**/*.{jpg,png,gif,svg}') )
-	.pipe( imagemin({
-		optimizationLevel: 3,
-		progressive: true,
-		interlaced: true,
-		multipass: true,
-		svgoPlugins: [{removeViewBox: false}],
-		use: [pngquant()]
-	}) )
+	.pipe(
+		cache( imagemin({
+			optimizationLevel: 3,
+			progressive: true,
+			interlaced: true,
+			multipass: true,
+			svgoPlugins: [{removeViewBox: false}],
+			use: [pngquant()]
+		}) )
+	)
 	.pipe(gulp.dest(filePaths.dest.img));
 }
 
+function watchImg() {
+	watch( path.join(filePaths.src.img, '**/*.{jpg,png,gif,svg}'), ['build:img'] );
+}
+
 gulp.task('build:img', buildImg);
+gulp.task('watch:img', watchImg);
 
 // -----------------------------------------------------------------------------
 // Images
 // -----------------------------------------------------------------------------
 
 function createBundle(bundler) {
-	return bundler.bundle()
-	.on('error', onError)
-	.pipe(source('main.js'))
-	.pipe( buffer() )
-	.pipe( rename({ suffix: '.min' }) )
-	.pipe( gulpif(production, uglify()) )
-	.pipe( gulp.dest(filePaths.dest.js) )
-	.pipe( livereload() );
+	del( path.join( filePaths.dest.js, '*.map') );
+	fs.access(filePaths.src.jsMain, fs.R_OK, function(err){
+		if (err)
+			return gutil.log ( gutil.colors.red.bold('No JS file found: ignoring javascript build') );
+		return bundler.bundle()
+			.on('error', onError)
+			.pipe( source(path.basename(filePaths.src.jsMain)) )
+			.pipe( buffer() )
+			.pipe( gulpif(!production, sourcemaps.init() ) )
+			.pipe( rename({ suffix: '.min' }) )
+			.pipe( gulpif(production, uglify()) )
+			.pipe( gulpif(!production, sourcemaps.write('.') ) )
+			.pipe( gulp.dest(filePaths.dest.js) )
+			.pipe( livereload() );
+	})
 }
 
 // -----------------------------------------------------------------------------
@@ -224,11 +235,11 @@ function createBundle(bundler) {
 // -----------------------------------------------------------------------------
 
 function buildJs(){
-	var bundler = browserify(filePaths.src.js);
+	var bundler = browserify(filePaths.src.jsMain);
 	createBundle(bundler);
 }
 function watchJs(){
-	var bundler = browserify(filePaths.src.js);
+	var bundler = browserify(filePaths.src.jsMain);
 	bundler.plugin(watchify, {ignoreWatch: ['**/node_modules/**', '**/bower_components/**']});
 	createBundle(bundler);
 	bundler.on('update', function(){
@@ -260,6 +271,7 @@ gulp.task('watch', function(){
 	startLivereolad();
 	watchCss();
 	watchJs();
+	watchImg();
 });
 
 gulp.task('default', ['webserver', 'watch']);
